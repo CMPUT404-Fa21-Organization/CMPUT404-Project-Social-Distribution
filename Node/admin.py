d22f5b222182487024ca400d2cf7da2009bf563f
Katherine Richards <37306581+kricha7ds@users.noreply.github.com>
2021-11-30 11:51:57 -0700

    complete sadmin control over node interactions

--- Node/admin.py
+++ Node/admin.py
@@ -3,7 +3,7 @@ from .models import Node
 
 # Register your models here.
 class NodeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'team', 'remote_server_url')
+    list_display = ('id', 'team', 'connected_server_url')
     ordering = ('team',)
 
 admin.site.register(Node, NodeAdmin)
--- Node/models.py
+++ Node/models.py
@@ -4,9 +4,9 @@ import uuid
 # Create your models here.
 class Node(models.Model):
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.CharField(primary_key=True, max_length=200, default=uuid.uuid4, editable=True)
     password = models.CharField(max_length=200, blank=False)
-    remote_server_url = models.URLField(max_length=200, default='', blank=True)
+    connected_server_url = models.URLField(max_length=200, default='', blank=True)
 
     CHOICES = [(i,i) for i in range(1,27)]
 
