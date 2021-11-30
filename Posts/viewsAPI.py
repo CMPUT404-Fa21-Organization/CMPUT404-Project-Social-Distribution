18b70f198e28fc1231bf3645e61b5dcea12edf8a
King Fu <xinjian@cs.ualberta.ca>
2021-11-30 01:35:39 -0700

    API add file and GET a specific post

--- Posts/viewsAPI.py
+++ Posts/viewsAPI.py
@@ -58,10 +58,12 @@ def newPost(request, uid=None, auth_pk=None):
 
         posts = Post(pk=uid, id=id, author_id=author_id, author=author, title=title, source=source, origin=origin, description=descirption, contentType=contentType, count=0, size=10, categories=categories,visibility=visibility, unlisted=unlisted, published=published, content=content, comments=comments_id)
         posts.save()
+        return True
     else:
         print(request.data)
         print(form.errors)
         print(form.data)
+        return False
 
 def add_Comment(request, post_pk, auth_pk=None, uid=None):
     form = CommentForm(request.POST, request.FILES)
@@ -139,17 +141,35 @@ def PostsList(request, auth_pk=None):
         page_size = 5
 
     if request.method == 'GET':
-        posts = Post.objects.all()
-        paginator = Paginator(posts, page_size)
-        page_obj = paginator.get_page(page_number)
-        serializer = PostSerializer(page_obj.object_list, many=True)
+        if auth_pk:
+            try:
+                author = Author.objects.get(auth_pk=auth_pk)
+                posts = Post.objects.filter(author_id=author)
+                code = status.HTTP_200_OK
+                paginator = Paginator(posts, page_size)
+                page_obj = paginator.get_page(page_number)
+                data = PostSerializer(page_obj.object_list, many=True).data
+            except Exception as e:
+                print(e)
+                data = {}
+                code = status.HTTP_400_BAD_REQUEST
+        else:
+            code = status.HTTP_200_OK
+            posts = Post.objects.all()
+            paginator = Paginator(posts, page_size)
+            page_obj = paginator.get_page(page_number)
+            data = PostSerializer(page_obj.object_list, many=True).data
 
     elif request.method == 'POST':
-        newPost(request, auth_pk=request.data['auth_pk'])
-        post = Post.objects.last()
-        serializer = PostSerializer(post)
+        if newPost(request, auth_pk=request.data['auth_pk']):
+            code = status.HTTP_202_ACCEPTED
+            post = Post.objects.last()
+            data = PostSerializer(post).data
+        else:
+            code = status.HTTP_400_BAD_REQUEST
+            data = {}
 
-    return Response(serializer.data)
+    return Response(data, code)
 
 @api_view(['GET', 'POST',])
 @authentication_classes([CustomAuthentication])
@@ -228,8 +248,14 @@ def PostDetail(request, post_pk, auth_pk=None):
                 post.unlisted = request.data['unlisted']
             if 'contentType' in request.data.keys():
                 post.contentType = request.data['contentType']
-            if 'text' in request.data.keys():
-                post.content = request.data['text']
+
+            if post.contentType == "application/app": 
+                post.content = request.FILES['file'].read() #Inputfile
+            elif post.contentType in ["image/png", "image/jpeg",]:
+                post.content = base64.b64encode(request.FILES['file'].read()) #Inputfile
+            else:
+                post.content = request.data["text"]
+
             post.save()
             serializer = PostSerializer(post)
         except Exception as e:
@@ -244,7 +270,7 @@ def PostDetail(request, post_pk, auth_pk=None):
     elif request.method == 'PUT':
         try:
             code = status.HTTP_200_OK
-            newPost(request, post_pk, request.data['auth_pk'])
+            assert newPost(request, post_pk, request.data['auth_pk'])==True
             post = Post.objects.get(post_pk=post_pk)
             serializer = PostSerializer(post)
         except Exception as e:
