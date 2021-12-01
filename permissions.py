81f4721de62621e731de5e92bfdf07620d0947e7
Neil <mparasha@ualberta.ca>
2021-11-30 20:21:43 -0700

    permissions revert

--- permissions.py
+++ permissions.py
@@ -2,26 +2,37 @@ from rest_framework import authentication, permissions
 from Node.models import Node
 import base64
 
+# class AccessPermission(permissions.BasePermission):
+#     def has_permission(self, request, view):
+#         auth_header = request.META.get('HTTP_AUTHORIZATION', '')
+#         token_type, _, credentials = auth_header.partition(' ')
+
+#         # decode incoming credentials
+#         decoded_credentials = base64.b64decode(credentials).decode("utf-8").split(':')
+
+#         try:        
+#             # check if key exists in db
+#             node = Node.objects.get(id=decoded_credentials[0])
+#         except:
+#             # if key not in db, reject request
+#             return False
+
+#         # identify the expected Auth pair, as stored in the db
+#         stored = f'{node.id}:{node.password}'
+
+#         # expected = base64.b64encode(b'socialdistribution_t14:c404t14').decode()
+#         expected = base64.b64encode(bytes(stored, 'UTF-8')).decode()
+#         if token_type == 'Basic' and credentials == expected:
+#             return True
+#         else:
+#             return False
+
 class AccessPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         auth_header = request.META.get('HTTP_AUTHORIZATION', '')
         token_type, _, credentials = auth_header.partition(' ')
 
-        # decode incoming credentials
-        decoded_credentials = base64.b64decode(credentials).decode("utf-8").split(':')
-
-        try:        
-            # check if key exists in db
-            node = Node.objects.get(id=decoded_credentials[0])
-        except:
-            # if key not in db, reject request
-            return False
-
-        # identify the expected Auth pair, as stored in the db
-        stored = f'{node.id}:{node.password}'
-
-        # expected = base64.b64encode(b'socialdistribution_t14:c404t14').decode()
-        expected = base64.b64encode(bytes(stored, 'UTF-8')).decode()
+        expected = base64.b64encode(b'socialdistribution_t14:c404t14').decode()
         if token_type == 'Basic' and credentials == expected:
             return True
         else:
@@ -32,21 +43,7 @@ class CustomAuthentication(authentication.BaseAuthentication):
         auth_header = request.META.get('HTTP_AUTHORIZATION', '')
         token_type, _, credentials = auth_header.partition(' ')
 
-        # decode incoming credentials
-        decoded_credentials = base64.b64decode(credentials).decode("utf-8").split(':')
-
-        try:        
-            # check if key exists in db
-            node = Node.objects.get(id=decoded_credentials[0])
-        except:
-            # if key not in db, reject request
-            return False
-
-        # identify the expected Auth pair, as stored in the db
-        stored = f'{node.id}:{node.password}'
-
-        # expected = base64.b64encode(b'socialdistribution_t14:c404t14').decode()
-        expected = base64.b64encode(bytes(stored, 'UTF-8')).decode()
+        expected = base64.b64encode(b'socialdistribution_t14:c404t14').decode()
         if token_type == 'Basic' and credentials == expected:
             return (True, None)
         else:
@@ -54,3 +51,31 @@ class CustomAuthentication(authentication.BaseAuthentication):
 
     def authenticate_header(self, request):
         return '{"username" : <username>, "password" : <password>}'
+
+# class CustomAuthentication(authentication.BaseAuthentication):
+#     def authenticate(self, request):
+#         auth_header = request.META.get('HTTP_AUTHORIZATION', '')
+#         token_type, _, credentials = auth_header.partition(' ')
+
+#         # decode incoming credentials
+#         decoded_credentials = base64.b64decode(credentials).decode("utf-8").split(':')
+
+#         try:        
+#             # check if key exists in db
+#             node = Node.objects.get(id=decoded_credentials[0])
+#         except:
+#             # if key not in db, reject request
+#             return False
+
+#         # identify the expected Auth pair, as stored in the db
+#         stored = f'{node.id}:{node.password}'
+
+#         # expected = base64.b64encode(b'socialdistribution_t14:c404t14').decode()
+#         expected = base64.b64encode(bytes(stored, 'UTF-8')).decode()
+#         if token_type == 'Basic' and credentials == expected:
+#             return (True, None)
+#         else:
+#             return None
+
+#     def authenticate_header(self, request):
+#         return '{"username" : <username>, "password" : <password>}'
