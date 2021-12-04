0139986e9c00a8ddce1ef1389b7beb07679581cf
Katherine Richards <37306581+kricha7ds@users.noreply.github.com>
2021-12-03 22:15:05 -0700

    fix profile github link and profile edit page

--- CMPUT404Project/settings.py
+++ CMPUT404Project/settings.py
@@ -109,6 +109,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "linkeddb",
+#         "USER": "postgres",
+#         "PASSWORD": "linked404",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
